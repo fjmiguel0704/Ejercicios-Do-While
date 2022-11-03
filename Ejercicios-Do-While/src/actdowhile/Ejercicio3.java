d9fdc17b0782efe9b82fc1c3a3c80666cb657611
fjmiguel <fjmiguel@106-16.info2>
2022-11-03 14:42:08 +0100

    Ejercicios-Do-While-T3

--- Ejercicios-Do-While/src/actdowhile/Ejercicio3.java
+++ Ejercicios-Do-While/src/actdowhile/Ejercicio3.java
@@ -6,6 +6,19 @@ public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		
+		int numero, suma=0, incremento=0;
+		
+		Scanner read = new Scanner (System.in);
+		System.out.println("Introduce el número del que quieras realizar la suma: ");
+		numero=read.nextInt();
+		
+		do {
+			incremento++;
+			suma+=incremento;
+		}
+		while(incremento<numero);
+		
+		System.out.println(suma);
 
 	}
 
